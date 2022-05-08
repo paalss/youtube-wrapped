7b9d0bda8550599c33e9722b8fb4befd32431cb2
paalss <paalss94@gmail.com>
2022-05-08 16:11:23 +0200

    added file upload to main test file

--- src/php/upload.php
+++ src/php/upload.php
@@ -1,25 +1,17 @@
 <?php
 $http_origin = $_SERVER['HTTP_ORIGIN'];
 header("Access-Control-Allow-Origin: $http_origin");
-// if ($http_origin == "http://www" || $http_origin == "http://localhost:8080") {
-//   header("Access-Control-Allow-Origin: $http_origin");
-// }
-// header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-// header("Access-Control-Allow-Headers: Origin");
-// header("Access-Control-Allow-Credentials: true");
-// header("Content-Type: application/json");
+header('Content-Type: application/json; charset=utf-8');
 
-// echo "<pre>";
-// ini_set('display_errors', 'On');
-// ini_set('html_errors', 0);
+if (is_uploaded_file($_FILES['uploadedFile']['tmp_name'])) {
+  $tmp_name = $_FILES['uploadedFile']['tmp_name'];
+  $name = $_FILES['uploadedFile']['name'];
 
-$data = $_POST['fileName']; // testdata.json
-// $data = $_POST['lastModified']; // 1650031382950
-// $data = $_POST['file']; // undefined index
-// $data = $_POST['val']; // undefined index
-// $data = $_FILES["ee"]["name"]; // undefined index
-// $data = [{filename: $_POST['fileName'] ee: $_POST['ee']}];
-
-
-echo json_encode($data);
-// echo "aaaaaaaaaaa";
+  if (move_uploaded_file($tmp_name, "uploads/$name")) {
+    echo "ok";
+  } else {
+    echo "fail";
+  }
+} else {
+  echo "noattempt";
+}
